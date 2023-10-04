6aa9dc8b4e54f2b7cd90f49c6af093f0f795e70e
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-10-04 02:14:31 +0000

    update app

--- Frontend/pages/_app.tsx
+++ Frontend/pages/_app.tsx
@@ -4,37 +4,33 @@ import { Navbar } from "../components/Navbar/Navbar";
 import NextNProgress from "nextjs-progressbar";
 import { NETWORK } from "../const/contractAddresses";
 import "../styles/globals.css";
-import { Metadata } from "next";
 import Footer from "../components/Footer/Footer";
-
-export const metadata: Metadata = {
-	title: "Digital Odyssey",
-	description: "The number one NFT marketplace in the world",
-	creator: "Kamasah Dickson",
-	keywords: ["NFT", "Web3", "NFT marketplace"],
-};
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<ThirdwebProvider
-			clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-			activeChain={NETWORK}
-		>
-			{/* Progress bar when navigating between pages */}
-			<NextNProgress
-				color="var(--color-tertiary)"
-				startPosition={0.3}
-				stopDelayMs={200}
-				height={3}
-				showOnShallow={true}
-			/>
+		<>
+			<Head>
+				<title>Digital Odyssey - NFT marketplace</title>
+				des
+			</Head>
+			<ThirdwebProvider
+				clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+				activeChain={NETWORK}
+			>
+				<NextNProgress
+					color="var(--color-tertiary)"
+					startPosition={0.3}
+					stopDelayMs={200}
+					height={3}
+					showOnShallow={true}
+				/>
 
-			{/* Render the navigation menu above each component */}
-			<Navbar />
-			{/* Render the actual component (page) */}
-			<Component {...pageProps} />
-			<Footer />
-		</ThirdwebProvider>
+				<Navbar />
+				<Component {...pageProps} />
+				<Footer />
+			</ThirdwebProvider>
+		</>
 	);
 }
 
