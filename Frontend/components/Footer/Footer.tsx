238cb451b2227d14e816ec0fe501ca016cca8bac
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-10-04 03:06:48 +0000

    update footer

--- Frontend/components/Footer/Footer.tsx
+++ Frontend/components/Footer/Footer.tsx
@@ -3,6 +3,9 @@ import styles from "./Footer.module.css";
 import logo from "../../public/logo.png";
 import Image from "next/image";
 import Link from "next/link";
+import { AiOutlineGithub } from "react-icons/ai";
+import { BsLinkedin } from "react-icons/bs";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
 	function getDate() {
@@ -28,9 +31,34 @@ const Footer = () => {
 					</Link>
 					✨
 				</div>
+				<ul className={styles.socialContainer}>
+					<li>
+						<Link
+							href={
+								"https://github.com/Kamasah-Dickson/Risein-Final-competition/tree/master"
+							}
+						>
+							<AiOutlineGithub size={25} />
+						</Link>
+					</li>
+					<li>
+						<Link
+							href={"https://www.linkedin.com/in/kamasah-dickson-8506a6230/"}
+						>
+							<BsLinkedin size={25} />
+						</Link>
+					</li>
+					<li>
+						<Link href={"https://twitter.com/Kamas_DEV"}>
+							<FaXTwitter size={25} />
+						</Link>
+					</li>
+				</ul>
 				<ul className={styles.list}>
 					<li>
 						<Link href={"/buy"}>Buy {`NFT's`}</Link>
+					</li>
+					<li>
 						<Link href={"/sell"}>Sell {`NFT's`}</Link>
 					</li>
 				</ul>
